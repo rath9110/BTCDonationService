75101f8a6c1ea0eb438b30efe59f057443136038
Rasmus Thunberg <rath701@gmail.com>
2025-11-23 15:50:39 +0100

    tried fixing issues

--- usdt-donation-router/hardhat.config.ts
+++ usdt-donation-router/hardhat.config.ts
@@ -1,6 +1,6 @@
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable, defineConfig } from "hardhat/config";
-import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
 
 export default defineConfig({
   plugins: [hardhatToolboxMochaEthersPlugin],
